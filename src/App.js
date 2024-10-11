85e8915ced4a4e317db93e4ba887150292e4cd97
Chandan <cpadhi521@gmail.com>
2024-10-11 21:39:00 +0530

    Implement navbar component styling and theme customization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload hiii.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/about" element={About} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
